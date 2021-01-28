f2075e8d2e6fd2e0c0a1e91e6e61c0a39e0331a1
fisermichal <michalfiser@seznam.cz>
2021-01-28 12:33:55 +0100

    add css,sass,js,images

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//
+//  DROPDOWN NAVBAR MENU ANIMATIONS
+//
+
+
+const hamburger = document.querySelector('.hamburger');
+const dropdown = document.querySelector('.dropdown-menu');
+const links = document.querySelectorAll('.dropdown-menu li');
+const aElement = document.querySelector('.hamburger a');
+
+hamburger.addEventListener('click', (e) => {
+
+    dropdown.classList.toggle('open');
+    aElement.classList.toggle('open');
+
+    links.forEach( link => {
+        link.classList.toggle('fade');
+    });
+
+    e.preventDefault();
+
+});
+
+
+//
+//  ON SCROLL STICKY NAVBAR
+//
+
+
+const header = document.querySelector('header');
+const headerOffset = header.offsetTop;
+
+    window.addEventListener('scroll', () => {
+
+        if ( pageYOffset > headerOffset ) {
+            header.classList.add('fromTheTop')
+        }
+        else if ( pageYOffset === headerOffset ) {
+            header.classList.remove('fromTheTop')
+        }
+
+    });
+
+
+//
+//  BACK TO TOP ARROW
+//
+
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+
+    if ( pageYOffset >= 100 ) {
+        backToTop.classList.add('show')
+    }
+    else {
+        backToTop.classList.remove('show')
+    }
+
+});
